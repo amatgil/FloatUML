f44be524c3d46ce7c5b66ce666f6eef8ae0f88dd
elena cai <elena@elena05>
2025-05-03 13:22:29 +0200

    rectangle contorno

--- src/umld_class.h
+++ src/umld_class.h
@@ -9,8 +9,9 @@ void umld_class(struct Classe c, struct Style *style) {
     uint32_t nattrs = c.attribs.len;
    
     Vector2 m_colon = MeasureTextEx(style->font, " : ", style->fontsize, 0);
-    Vector2 size_rect = {m_colon.x, (nattrs + 1) * m_colon.y};
-
+    uint32_t marge = (m_colon.y/3);
+    Vector2 size_rect = {m_colon.x*1.33, (nattrs + 1) * m_colon.y + marge};
+    
     for (uint32_t i = 0; i < nattrs; ++i) {
         Vector2 m_nom = MeasureTextEx(style->font, c.attribs.attrs[i].nom.text,
             style->fontsize, 0);
@@ -18,12 +19,12 @@ void umld_class(struct Classe c, struct Style *style) {
             style->font, c.attribs.attrs[i].tipus.text, style->fontsize, 0);
 
         if (m_nom.x + m_typ.x + m_colon.x > max) {
-            max = m_nom.x + m_typ.x + m_colon.x;
-            size_rect.x = m_nom.x + m_typ.x + m_colon.x;
+            max = m_nom.x + m_typ.x + m_colon.x*1.33;
+            size_rect.x = m_nom.x + m_typ.x + m_colon.x*1.33;
         }
     }   
     DrawRectangleV(c.pos, size_rect, WHITE);
-    DrawTextEx(style->font, c.nom, c.pos, style->fontsize, 0, BLACK);
+    DrawTextEx(style->font, c.nom, (Vector2){c.pos.x + marge/2, c.pos.y + marge/2}, style->fontsize, 0, BLACK);
     for (uint32_t i = 0; i < nattrs; ++i) {
 
         Vector2 m_nom = MeasureTextEx(style->font, c.attribs.attrs[i].nom.text,
@@ -31,7 +32,7 @@ void umld_class(struct Classe c, struct Style *style) {
         Vector2 m_typ = MeasureTextEx(
             style->font, c.attribs.attrs[i].tipus.text, style->fontsize, 0);
 
-        Vector2 pos = {c.pos.x, c.pos.y + i * m_nom.y + m_nom.y};
+        Vector2 pos = {c.pos.x + marge/2, c.pos.y + i * m_nom.y + m_nom.y + marge};
 
         DrawTextEx(style->font, c.attribs.attrs[i].nom.text, pos,
                    style->fontsize, 0, BLACK);
@@ -42,7 +43,7 @@ void umld_class(struct Classe c, struct Style *style) {
         DrawTextEx(style->font, c.attribs.attrs[i].tipus.text, pos,
                    style->fontsize, 0, BLACK);
     }
-    Vector2 p_f = {c.pos.x, c.pos.y + (nattrs + 1) * m_colon.y};
+    Vector2 p_f = {c.pos.x, c.pos.y + (nattrs + 1) * m_colon.y + marge};
     DrawLineEx(c.pos, p_f, 3.5, BLACK); // vertical 1
 
     p_f = (Vector2){c.pos.x + max, c.pos.y};
@@ -50,13 +51,13 @@ void umld_class(struct Classe c, struct Style *style) {
 
     p_f = (Vector2){c.pos.x + max, c.pos.y + m_colon.y};
     Vector2 p_i = {c.pos.x, c.pos.y + m_colon.y};
-    DrawLineEx(p_i, p_f, 3.5, BLACK); // horitzontal 2
+    DrawLineEx(p_i, p_f, 3, BLACK); // horitzontal 2
 
-    p_f = (Vector2){c.pos.x + max, c.pos.y + (nattrs + 1) * m_colon.y};
-    p_i = (Vector2){c.pos.x, c.pos.y + (nattrs + 1) * m_colon.y};
+    p_f = (Vector2){c.pos.x + max, c.pos.y + (nattrs + 1) * m_colon.y + marge};
+    p_i = (Vector2){c.pos.x, c.pos.y + (nattrs + 1) * m_colon.y + m_colon.y/3};
     DrawLineEx(p_i, p_f, 3.5, BLACK); // horitzontal 3
 
-    p_f = (Vector2){c.pos.x + max, c.pos.y + (nattrs + 1) * m_colon.y};
+    p_f = (Vector2){c.pos.x + max, c.pos.y + (nattrs + 1) * m_colon.y + marge};
     p_i = (Vector2){c.pos.x + max, c.pos.y};
     DrawLineEx(p_i, p_f, 3.5, BLACK); // vertical 2
 
--- src/utils.h
+++ src/utils.h
@@ -11,7 +11,7 @@ Rectangle umld_rect_of(struct Classe c, struct Style *style) {
     uint32_t max = 0;
     uint32_t nattrs = c.attribs.len;
     Vector2 m_colon = MeasureTextEx(style->font, " : ", style->fontsize, 0);
-
+    uint32_t marge = (m_colon.y/3);
     for (uint32_t i = 0; i < nattrs; ++i) {
 
         Vector2 m_nom = MeasureTextEx(style->font, c.attribs.attrs[i].nom.text,
@@ -22,7 +22,7 @@ Rectangle umld_rect_of(struct Classe c, struct Style *style) {
         Vector2 pos = {c.pos.x, c.pos.y + i * m_nom.y + m_nom.y};
 
         if (m_nom.x + m_typ.x + m_colon.x > max)
-            max = m_nom.x + m_typ.x + m_colon.x;
+            max = m_nom.x + m_typ.x + m_colon.x*1.33;
     }
-    return (Rectangle){c.pos.x, c.pos.y, max, m_colon.y * (nattrs + 1)};
+    return (Rectangle){c.pos.x, c.pos.y, max, m_colon.y * (nattrs + 1) + marge};
 }
