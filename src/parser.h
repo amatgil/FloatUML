c97677c8854f53055a6732aac245020032894ab3
amatgil <amatgilvinyes@gmail.com>
2025-05-04 04:09:36 +0200

    First pass at parsing associativa

--- src/parser.h
+++ src/parser.h
@@ -70,13 +70,23 @@ int32_t parse(struct StrSlice *a, struct World *w) {
         } else if (umls_cmp_cstr(&word, "rel")) {
             if (umls_cmp_cstr(&word, ""))
                 return -1;
-            // rel { A 0 0 B 1 1 }
+            // rel assname { A 0 0 B 1 1 C 2 2 }
+            struct Relacio parsed_rel = umlr_init();
 
             struct StrSlice lbracket = umlss_readw(&b);
-            if (!umls_cmp_cstr(&lbracket, "{"))
-                return -1;
+            if (!umls_cmp_cstr(&lbracket, "{")) {
+                // We assume it's a name, but we must follow up with a bracket
+                int k = search_class(&parsed_classes, lbracket.text);
+                if (k == -1)
+                    return -1;
+                else
+                    parsed_rel.associativa = &parsed_classes.cs[k];
 
-            struct Relacio parsed_rel = umlr_init();
+                lbracket = umlss_readw(&b);
+                if (!umls_cmp_cstr(&lbracket, "{"))
+                    return -1;
+            }
+
             while (1) {
                 struct StrSlice nom = umlss_readw(&b);
                 if (umls_cmp_cstr(&nom, "}"))
@@ -130,6 +140,16 @@ int32_t parse(struct StrSlice *a, struct World *w) {
         // world)
         for (int i = 0; i < parsed_relacions.len; ++i) {
             struct Relacio rel = parsed_relacions.rs[i];
+            // the associativa of the relacio
+            if (rel.associativa != NULL) {
+                int k = search_class(&w->classes, rel.associativa->nom);
+                if (k == -1)
+                    return -1;
+                else
+                    rel.associativa = &w->classes.cs[k];
+            }
+
+            // All the classes that form la relacio
             for (int j = 0; j < rel.len; ++j) {
                 struct Classe *class_in_rel = rel.cs[j];
                 int k = search_class(&w->classes, class_in_rel->nom);
