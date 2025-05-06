8950518fb556484dbe87f3f2d79017639d051e04
amatgil <amatgilvinyes@gmail.com>
2025-05-06 17:37:27 +0200

    Add another parse function

--- src/parser.rs
+++ src/parser.rs
@@ -2,20 +2,39 @@
 //! We only parse &str here
 //! (Accepts full utf8!!)
 
-type ParserRes<'a, T> = Result<(T, &'a str), ()>;
+use crate::Classe;
+
+type ParserRes<'a, T> = Option<(T, &'a str)>;
 
 fn parse_letter(input: &str, c: char) -> ParserRes<&str> {
+    let input = input.trim_start();
     match input.strip_prefix(c) {
-        Some(rest) => Ok((&input[0..c.len_utf8()], rest)),
-        None => Err(()),
+        Some(rest) => Some((&input[0..c.len_utf8()], rest)),
+        None => None,
     }
 }
 
 fn parse_word<'a>(input: &'a str, word: &'a str) -> ParserRes<'a, &'a str> {
+    let input = input.trim_start();
     match input.strip_prefix(word) {
-        Some(rest) => Ok((&input[0..word.bytes().len()], rest)),
-        None => Err(()),
+        Some(rest) => Some((&input[0..word.bytes().len()], rest)),
+        None => None,
     }
+}
+
+fn parse_until<P>(input: &str, pred: P) -> ParserRes<&str>
+where
+    P: Fn(char) -> bool,
+{
+    match input.char_indices().find(|(_i, c)| pred(*c)) {
+        Some((i, _c)) => Some((&input[0..i], &input[i..])),
+        None => None,
+    }
+}
+
+fn parse_classe(input: &str) -> ParserRes<Classe> {
+    let (_classe, input) = parse_word(input, "classe")?;
+    todo!()
 }
 
 #[test]
@@ -28,6 +47,14 @@ fn letter_test() {
     assert_eq!(b, "b");
     assert_eq!(c, "c");
     assert_eq!(input, "de")
+}
+
+#[test]
+fn until_test() {
+    let input = "abcd:fgh";
+    let (parsed, input) = parse_until(input, |c| c == ':').unwrap();
+    assert_eq!(parsed, "abcd");
+    assert_eq!(input, ":fgh");
 }
 
 #[test]
