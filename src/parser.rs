84a16e5f4f1f8b127893d86a4d55fa8c9bde629b
amatgil <amatgilvinyes@gmail.com>
2025-05-06 18:16:26 +0200

    Prepare attrib parsing

--- src/parser.rs
+++ src/parser.rs
@@ -2,12 +2,14 @@
 //! We only parse &str here
 //! (Accepts full utf8!!)
 
-use crate::Classe;
+use raylib::math::Vector2;
+
+use crate::{Attribute, Classe};
 
 type ParserRes<'a, T> = Option<(T, &'a str)>;
 
 fn parse_letter(input: &str, c: char) -> ParserRes<&str> {
-    let input = input.trim_start();
+    let input = input.trim();
     match input.strip_prefix(c) {
         Some(rest) => Some((&input[0..c.len_utf8()], rest)),
         None => None,
@@ -15,9 +17,9 @@ fn parse_letter(input: &str, c: char) -> ParserRes<&str> {
 }
 
 fn parse_word<'a>(input: &'a str, word: &'a str) -> ParserRes<'a, &'a str> {
-    let input = input.trim_start();
+    let input = input.trim();
     match input.strip_prefix(word) {
-        Some(rest) => Some((&input[0..word.bytes().len()], rest)),
+        Some(rest) => Some((&input[0..word.bytes().len()], rest.trim())),
         None => None,
     }
 }
@@ -26,14 +28,56 @@ fn parse_until<P>(input: &str, pred: P) -> ParserRes<&str>
 where
     P: Fn(char) -> bool,
 {
+    let input = input.trim();
     match input.char_indices().find(|(_i, c)| pred(*c)) {
-        Some((i, _c)) => Some((&input[0..i], &input[i..])),
+        Some((i, _c)) => Some((&input[0..i], &input[i..].trim())),
         None => None,
     }
 }
 
-fn parse_classe(input: &str) -> ParserRes<Classe> {
+pub fn parse_classe(input: &str) -> ParserRes<Classe> {
+    let input = input.trim();
     let (_classe, input) = parse_word(input, "classe")?;
+    let (_, input) = parse_word(input, "{")?;
+    let (nom, mut input) = parse_until(input, |c| c == ' ')?;
+
+    let mut attribs = vec![];
+    while let Some((at, inputt)) = parse_attrib(input) {
+        attribs.push(at);
+        input = inputt;
+    }
+    let (_, _input) = parse_word(input, "}")?;
+
+    Some((
+        Classe {
+            nom: nom.to_string(),
+            attribs,
+            pos: Vector2 { x: 0.0, y: 0.0 },
+            superclass: None, // TODO: Superclasses parsing
+        },
+        input,
+    ))
+}
+
+pub fn parse_attrib(input: &str) -> ParserRes<Attribute> {
+    let input = input.trim();
+    let (nom, input) = parse_until(input, |c| c == ' ' || c == ':')?;
+    let (tipus, input) = parse_until(input, |c| c == ' ')?;
+    let (multmin, input) = parse_int(input)?;
+    let (multmax, input) = parse_int(input)?;
+
+    Some((
+        Attribute {
+            nom: nom.to_string(),
+            tipus: tipus.to_string(),
+            multmin: u32::try_from(multmin).ok(),
+            multmax: u32::try_from(multmax).ok(),
+        },
+        input,
+    ))
+}
+
+fn parse_int(input: &str) -> ParserRes<i32> {
     todo!()
 }
 
